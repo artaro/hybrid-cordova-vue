e7ff0d6c6e2d754b9e7cd8fc50f3d11a4536b84c
Pumin Swangjang <fvck3oy@hotmail.com>
2018-11-26 13:20:56 +0700

    set up vue with firebase from fvck3oy

--- src/main.js
+++ src/main.js
@@ -1,15 +1,45 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import Vue from 'vue'
+// import App from './App'
+// import router from './router'
+
+// Vue.config.productionTip = false
+
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import firebase from 'firebase'
+import './components/firebaseInit'
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+let app
+// var config = {
+//   apiKey: 'AIzaSyAVqxz9IAzrH7-XEK1huZNrRNHHoPuoXqk',
+//   authDomain: 'webapp-ac16b.firebaseapp.com',
+//   databaseURL: 'https://webapp-ac16b.firebaseio.com',
+//   projectId: 'webapp-ac16b',
+//   storageBucket: 'webapp-ac16b.appspot.com',
+//   messagingSenderId: '42883641659'
+// }
+// firebase.initializeApp(config)
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      el: '#app',
+      template: '<App/>',
+      components: { App },
+      router
+    })
+  }
 })
