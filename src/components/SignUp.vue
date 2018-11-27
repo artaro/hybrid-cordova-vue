f1af684a41262436fe0c351e58eb6b7595efe930
Pumin Swangjang <fvck3oy@hotmail.com>
2018-11-28 02:48:49 +0700

    fixed color - sign up

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,35 +2,38 @@
   <div class="sign-up">
     <!-- <img src=".././assets/logoFood.png" style="width:260px;height:260px;"> -->
     <p class="title">Let's create a new account !</p>
-    <input style="	
-    color: rgb(55, 123, 52);
+   	<input style="	
+color: #a7744d ;
     font-size: 18px;
-    width: 250px !important;
+    width: 270px !important;
     height: 40px;
-    border-radius: 20px !important;
+		
+    <!-- border-radius: 20px !important; -->
     border: 0.5px solid rgb(82, 212, 74);
     padding: 4px 0px 4px 19px;
-    box-shadow: rgb(75, 197, 96) 3px 3px 10px -3px;
-    outline-width: 0px;" type="text" v-model="email" placeholder="Email"><br>
-    <input style="	
-    color: rgb(55, 123, 52);
+    <!-- box-shadow: rgb(75, 197, 96) 3px 3px 10px -3px; -->
+    outline-width: 0px; " type="text" v-model="email" placeholder="Email"><br>
+    	<input style="	
+color: #a7744d ;
     font-size: 18px;
-    width: 250px !important;
+    width: 270px !important;
     height: 40px;
-    border-radius: 20px !important;
+		
+    <!-- border-radius: 20px !important; -->
     border: 0.5px solid rgb(82, 212, 74);
     padding: 4px 0px 4px 19px;
-    box-shadow: rgb(75, 197, 96) 3px 3px 10px -3px;
-    outline-width: 0px;" type="password" v-model="password" placeholder="Password"><br>
+    <!-- box-shadow: rgb(75, 197, 96) 3px 3px 10px -3px; -->
+    outline-width: 0px; " type="password" v-model="password" placeholder="Password"><br>
     <button style="	font-size: 18px;
 	width: 270px !important;
 	height: 49px;
-	border-radius: 20px !important;
+		font-weight:bold;
+	<!-- border-radius: 20px !important; -->
 	border: 0.5px solid rgb(76, 171, 71);
 	padding: 4px 0px 4px 0px;
 	box-shadow: 3px 3px 10px -3px #cacaca;
   outline-width: 0px;
-  background-color: rgb(76, 171, 71);
+  background-color: #ff9052;
   color: #fff;
   " v-on:click="signUp">Sign Up</button>
     <div class="title2">or go back to
@@ -77,6 +80,7 @@ export default {
 }
 
 input {
+	color: #f0c791;
 	margin: 10px 0;
 	width: 20%;
 	padding: 15px;
